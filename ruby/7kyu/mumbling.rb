c243373959a799de2d67adc64bc7081c03005e8e
Andrés Del Carpio <andresdc@andresdc.net>
2018-11-15 17:03:25 -0500

    two exercises added

---
+++ ruby/7kyu/mumbling.rb
@@ -0,0 +1,27 @@
+# Mumbling
+# https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/solutions/ruby
+# The examples below show you how to write function accum:
+# Examples:
+
+# accum("abcd")    # "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") # "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt")    # "C-Ww-Aaa-Tttt"
+
+# The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+def accum(s)
+  i = 1
+  str = ""
+  while i <= s.length
+    if i != 1
+      str << "-"
+    end
+    str << "#{s[i - 1] * i}".capitalize
+    i += 1
+  end
+  p str
+end
+
+accum("abcd")    # "A-Bb-Ccc-Dddd"
+accum("RqaEzty") # "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt")    # "C-Ww-Aaa-Tttt"
