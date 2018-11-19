55e5ba83e54cfe09bfda19a18024c0a8cfe75ea6
Andrés Del Carpio <andresdc@andresdc.net>
2018-11-19 13:26:05 -0500

    added url for tribonacci

--- ruby/6kyu/tribonacci.rb
+++ ruby/6kyu/tribonacci.rb
@@ -1,5 +1,5 @@
 # Tribonacci Sequence
-
+# https://www.codewars.com/kata/tribonacci-sequence/solutions/ruby
 # As the name may already reveal, it works basically like a Fibonacci, but
 # summing the last 3 (instead of 2) numbers of the sequence to generate the
 # next.
