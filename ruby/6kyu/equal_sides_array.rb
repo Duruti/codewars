bb33e2866119bf42cd51ef05b4f9f1bfe6425471
Andrés Del Carpio <andresdc@andresdc.net>
2018-11-19 17:11:09 -0500

    add equal side of an array solution

---
+++ ruby/6kyu/equal_sides_array.rb
@@ -0,0 +1,62 @@
+# Equal Sides Of An Array
+# https://www.codewars.com/kata/equal-sides-of-an-array/solutions/ruby
+# You are going to be given an array of integers. Your job is to take that
+# array and find an index N where the sum of the integers to the left of N is
+# equal to the sum of the integers to the right of N. If there is no index that
+# would make this happen, return -1.
+
+# For example:
+
+# Let's say you are given the array {1,2,3,4,3,2,1}:
+# Your function will return the index 3, because at the 3rd position of the
+# array, the sum of left side of the index ({1,2,3}) and the sum of the right
+# side of the index ({3,2,1}) both equal 6.
+
+# Let's look at another one.
+# You are given the array {1,100,50,-51,1,1}:
+# Your function will return the index 1, because at the 1st position of the
+# array, the sum of left side of the index ({1}) and the sum of the right side
+# of the index ({50,-51,1,1}) both equal 1.
+
+# Last one:
+# You are given the array {20,10,-80,10,10,15,35}
+# At index 0 the left side is {}
+# The right side is {10,-80,10,10,15,35}
+# They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+# Index 0 is the place where the left side and right side are equal.
+
+# Note: Please remember that in most programming/scripting languages the index
+# of an array starts at 0.
+
+# Input:
+# An integer array of length 0 < arr < 1000. The numbers in the array can be
+# any integer positive or negative.
+
+# Output:
+# The lowest index N where the side to the left of N is equal to the side to
+# the right of N. If you do not find an index that fits these rules, then you
+# will return -1.
+
+# Note:
+# If you are given an array with multiple answers, return the lowest correct index.
+# An empty array should be treated like a 0 in this problem.
+
+def find_even_index(a)
+  a.each_index do |x|
+    if a[0...x].inject(0, :+) == a[x + 1..a.length - 1].inject(0, :+)
+      return x
+    end
+  end
+  return -1
+end
+
+find_even_index([1,2,3,4,3,2,1]) # 3
+find_even_index([1,100,50,-51,1,1]) # 1
+find_even_index([1,2,3,4,5,6]) # -1
+find_even_index([20,10,30,10,10,15,35]) # 3
+find_even_index([20,10,-80,10,10,15,35]) # 0
+find_even_index([10,-80,10,10,15,35,20]) # 6
+find_even_index(Array(1..100)) # -1
+find_even_index([0,0,0,0,0]) # 0,"Should pick the first index if more cases are valid"
+find_even_index([-1,-2,-3,-4,-3,-2,-1]) # 3
+find_even_index(Array(-100..-1)) # -1
