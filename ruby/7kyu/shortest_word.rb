94a56ba3e9e66b71d423ba32df2640b430882495
Andres Del Carpio <andresdc@andresdc.net>
2018-11-21 17:18:24 -0500

    add shortest word

---
+++ ruby/7kyu/shortest_word.rb
@@ -0,0 +1,17 @@
+# Shortest Word
+
+# Simple, given a string of words, return the length of the shortest word(s).
+
+# String will never be empty and you do not need to account for different data
+# types.
+
+def find_short(s)
+  a = s.split.uniq.map { |x| x.length }
+  return a.uniq.sort[0]
+end
+
+find_short("bitcoin take over the world maybe who knows perhaps") # 3
+find_short("turns out random test cases are easier than writing out basic ones") # 3
+find_short("lets talk about javascript the best language") # 3
+find_short("i want to travel the world writing code one day") # 1
+find_short("Lets all go on holiday somewhere very cold") # 2
