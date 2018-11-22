b09f6575719f9d9acc41976b08fd9f6d0f178827
Andres Del Carpio <andresdc@andresdc.net>
2018-11-22 12:50:04 -0500

    add link for shortest_word solutions

--- ruby/7kyu/shortest_word.rb
+++ ruby/7kyu/shortest_word.rb
@@ -1,5 +1,5 @@
 # Shortest Word
-
+# https://www.codewars.com/kata/shortest-word/solutions/ruby
 # Simple, given a string of words, return the length of the shortest word(s).
 
 # String will never be empty and you do not need to account for different data
