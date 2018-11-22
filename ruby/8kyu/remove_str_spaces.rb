de42b6d14a1186b3204f56b81769f85a9a608ecd
Andres Del Carpio <andresdc@andresdc.net>
2018-11-22 16:27:43 -0500

    refactor exercise solution with regex

--- ruby/8kyu/remove_str_spaces.rb
+++ ruby/8kyu/remove_str_spaces.rb
@@ -3,7 +3,7 @@
 # Simple, remove the spaces from the string, then return the resultant string.
 
 def no_space(x)
-  x.gsub(" ", "")
+  x.gsub(/\s/, "")
 end
 
 no_space('8 j 8   mBliB8g  imjB8B8  jl  B') # '8j8mBliB8gimjB8B8jlB'
