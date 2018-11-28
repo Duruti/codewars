e0c8334eb0a146ec80637d91ed63e28e83fd8a35
Andres Del Carpio <andresdc@andresdc.net>
2018-11-28 14:38:15 -0500

    refactor

--- ruby/7kyu/min_max.rb
+++ ruby/7kyu/min_max.rb
@@ -5,6 +5,6 @@ def min_max(lst)
   [lst.min, lst.max]
 end
 
-min_max([1,2,3,4,5]) # [1,5]
-min_max([2334454,5]) # [5, 2334454]
+min_max([1, 2, 3, 4, 5]) # [1,5]
+min_max([2334454, 5]) # [5, 2334454]
 min_max([1]) # [1, 1]
--- ruby/7kyu/sum_odds.rb
+++ ruby/7kyu/sum_odds.rb
@@ -2,17 +2,7 @@
 # https://www.codewars.com/kata/sum-of-odd-numbers/ruby
 
 def row_sum_odd_numbers(num)
-  # store the sum of number up to the given number and get the first number
-  c = 0
-  s = 0
-  1.upto(num - 1) { |i| c += i }
-  c = c * 2 + 1
-  # sum the next n odd numbers
-  (1..num).each do
-    s += c
-    c += 2
-  end
-  s
+  num**3
 end
 
 row_sum_odd_numbers(1) # 1
