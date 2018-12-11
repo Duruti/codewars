1d23da125eaf7e37065774b0107ee369375710ef
Andres Del Carpio <andresdc@andresdc.net>
2018-12-11 13:15:23 -0500

    formatting changes

--- ruby/7kyu/sum_multiples_3_5.rb
+++ ruby/7kyu/sum_multiples_3_5.rb
@@ -2,7 +2,7 @@
 # https://www.codewars.com/kata/sum-of-all-the-multiples-of-3-or-5/ruby
 
 def find(num)
-  p (3..num).select { |i| (i % 3).zero? || (i % 5).zero? }.reduce(:+)
+  (3..num).select { |i| (i % 3).zero? || (i % 5).zero? }.reduce(:+)
 end
 
 find(5) # 8
