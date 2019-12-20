fe1fe710c3ce9b28d236fd507d80a6484f2d5e28
Andrés Del Carpio <andresdc@andresdc.net>
2019-12-20 17:17:59 -0500

    Added DNA solutions to Ruby and Javascript

---
+++ javascript/7kyu/dna.js
@@ -0,0 +1,7 @@
+// Complementary DNA
+// https://www.codewars.com/kata/complementary-dna/javascript
+
+function DNAStrand(dna){
+  const dnaReplacement = { A: 'T', T: 'A', C: 'G', G: 'C' }
+  return dna.split('').map( e => dnaReplacement[e] ).join('');
+}
