172f3092b727717e2919c517052ea0e76d1a529e
Andres Del Carpio <andresdc@andresdc.net>
2020-02-20 09:11:06 -0500

    Formated with prettier

--- javascript/6kyu/to-camel-case.js
+++ javascript/6kyu/to-camel-case.js
@@ -1,6 +1,6 @@
-function toCamelCase(str){
+function toCamelCase(str) {
   return str
     .split(/[-_]/)
-    .map((e, i) => i !== 0 ? e.charAt(0).toUpperCase() + e.slice(1) : e)
-    .join('')
+    .map((e, i) => (i !== 0 ? e.charAt(0).toUpperCase() + e.slice(1) : e))
+    .join("");
 }
