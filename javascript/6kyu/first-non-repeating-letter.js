618b8f7a7abeac99332c2a3e6b325d4b691f8917
Andrés Del Carpio <andresdc@andresdc.net>
2020-03-02 03:46:36 -0500

    Added links to katas

--- javascript/6kyu/first-non-repeating-letter.js
+++ javascript/6kyu/first-non-repeating-letter.js
@@ -1,3 +1,6 @@
+// First non-repeating character
+// https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+
 function firstNonRepeatingLetter(s) {
   return s.toLowerCase().lastIndexOf(s.toLowerCase().charAt(0)) !== 0
     ? firstNonRepeatingLetter(s.replace(new RegExp(s.charAt(0), 'g'), ''))
--- javascript/6kyu/to-camel-case.js
+++ javascript/6kyu/to-camel-case.js
@@ -1,3 +1,6 @@
+// Convert string to camel case
+// https://www.codewars.com/kata/517abf86da9663f1d2000003/javascript
+
 function toCamelCase(str) {
   return str
     .split(/[-_]/)
