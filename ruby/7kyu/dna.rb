f0120831fb4518ef0e3ae7636c394b430667a45c
Andrés Del Carpio <andresdc@andresdc.net>
2020-03-03 01:53:30 -0500

    Added DNA solution

--- ruby/7kyu/dna.rb
+++ ruby/7kyu/dna.rb
@@ -1,7 +1,7 @@
 # Complementary DNA
 # https://www.codewars.com/kata/complementary-dna/ruby
 
-def DNA_strand(dna)
+def dna_strand(dna)
   dna_replacement = { 'A' => 'T', 'T' => 'A', 'C' => 'G', 'G' => 'C' }
   dna.split('').map { |x| dna_replacement[x] }.join('')
 end
